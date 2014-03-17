cf3a69679a15cad50302f962d7439202aecdc8fc
Giorgos Logiotatidis <seadog@sealabs.net>
2014-03-17 09:31:52 +0200

    Remove 'requires' from setup.py
    Parameter 'install_requires' install flake8 so we can safely remove
    'requires'.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
 setup(
-    name = 'imhotep_flake8',
-    packages = ['imhotep_flake8'], 
-    version = '0.2.0',
-    description = 'An imhotep plugin for flake8 validation.',
-    author = 'Giorgos Logiotatidis',
-    author_email = 'giorgos@mozilla.com',
-    url = 'https://github.com/glogiotatidis/imhotep_flake8',
-    keywords = ['testing', 'imhotep'], 
-    classifiers = [],
+    name='imhotep_flake8',
+    packages=['imhotep_flake8'],
+    version='0.2.0',
+    description='An imhotep plugin for flake8 validation.',
+    author='Giorgos Logiotatidis',
+    author_email='giorgos@mozilla.com',
+    url='https://github.com/glogiotatidis/imhotep_flake8',
+    keywords=['testing', 'imhotep'],
+    classifiers=[],
     install_requires=['flake8'],
-    requires=['flake8'],
     entry_points={
         'imhotep_linters': [
             '.py = imhotep_flake8.plugin:Flake8Linter'
